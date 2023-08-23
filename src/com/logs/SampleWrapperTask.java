0860269c5bfcdd38108e6bdc52cbe349ab792d29
Facundo Perez Schneider <facups200718@gmail.com>
2023-08-23 00:51:14 -0300

    Wrapper classes

--- src/com/logs/SampleWrapperTask.java
+++ src/com/logs/SampleWrapperTask.java
@@ -1,4 +1,15 @@
 package com.logs;
 
 public class SampleWrapperTask {
+    public static void main(String[] args) {
+        // Primitive to wrapper
+        int number = 120;
+        Integer object = Integer.valueOf(number);
+        double number2 = 120.12;
+        Double object2 = Double.valueOf(number2);
+
+        // Wrapper to primitive
+        int number3 = object.intValue();
+        double number4 = object2.doubleValue();
+    }
 }
