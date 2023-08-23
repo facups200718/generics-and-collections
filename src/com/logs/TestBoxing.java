4d167a090421db89279ce2df1fd85e22cfbcbf41
Facundo Perez Schneider <facups200718@gmail.com>
2023-08-23 01:02:41 -0300

    Autoboxing y autounboxing

---
+++ src/com/logs/TestBoxing.java
@@ -0,0 +1,14 @@
+package com.logs;
+
+import java.util.ArrayList;
+
+public class TestBoxing {
+    public static void main(String[] args) {
+        char data = 'a';
+        Character character = data;
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(10);
+        Integer dataValue = 10;
+        int value = dataValue;
+    }
+}
